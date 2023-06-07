45cd79f8407b52a066c1dad0c576eb573811913b
Chris Kayahara <kayahara.christopher@gmail.com>
2023-06-07 16:45:47 -0400

    Setup dual slider for playlist pace selection

--- src/components/DualRange/DualRange.tsx
+++ src/components/DualRange/DualRange.tsx
@@ -1,13 +1,33 @@
 import { useState } from "react";
 
+import "./DualRange.scss";
+
 export default function DualRange() {
   const [upperPace, setUpperPace] = useState(50);
   const [lowerPace, setLowerPace] = useState(100);
 
   return (
-    <div>
-      <input type="range"></input>
-      <input type="range"></input>
+    <div className="dual-range">
+      <div className="dual-range__track"></div>
+      <div className="dual-range__range"></div>
+      <input
+        className="dual-range__slider-lower"
+        type="range"
+        min="1"
+        max="50"
+      ></input>
+      <input
+        className="dual-range__slider-upper"
+        type="range"
+        min="50"
+        max="100"
+      ></input>
+      <input
+        className="dual-range__slider-center"
+        type="range"
+        min="50"
+        max="100"
+      ></input>
     </div>
   );
 }
