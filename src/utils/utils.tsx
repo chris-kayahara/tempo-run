bfcaaf7483d08033eed97a9e7bddc1f2ac62c06f
Chris Kayahara <kayahara.christopher@gmail.com>
2023-07-10 14:16:09 -0400

    Add playlist data (eg. total length, track count, and total steps)

---
+++ src/utils/utils.tsx
@@ -0,0 +1,11 @@
+export const msToTime = (ms: number) => {
+  const hours = Math.floor(ms / 1000 / 3600);
+  const minutes = Math.floor((ms / 1000 / 60) % 60);
+  const seconds = Math.round((ms / 1000) % 60);
+  // prettier-ignore
+  return hours > 0
+    ? hours + ":" +(minutes < 10 ? "0" : "") + minutes + ":" + (seconds < 10 ? "0" : "") + seconds
+    : hours < 1
+    ? minutes + ":" + (seconds < 10 ? "0" : "") + seconds
+    : "Error";
+};
